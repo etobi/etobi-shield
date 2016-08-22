3b6291d16506ef7e42d3e2c91ed677bc3167a2d7
Tobias Liebig <work@etobi.de>
2016-08-23 00:17:19 +0200

    [TASK] readAdcValue & readAdcTemp

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 
-exports.helloworld = function() {
-  console.log("Hello World");
-}
+exports.readAdcValue = function (channel, callback) {
+	// TODO
+	var max = 620,
+			min = 590;
+	var value = Math.floor(Math.random() * (max - min + 1) + min);
+	if (callback) {
+		callback(channel, value);
+	}
+};
 
+exports.readAdcTemp = function (probe, callback) {
+
+	var channel = probe.channel;
+
+	etobiShield.readAdcValue(channel, function (channel, value) {
+		if (value === 0) return 999.9;
+
+		var tempResistor = probe.resistor * ((probe.maxvalue / value) - 1);
+		var volts = Math.log(tempResistor / probe.rn);
+
+		// steinhart-hart
+		var temp = (1 / (probe.a + (probe.b * volts) + (probe.c * volts * volts))) - 273;
+
+		if (probe.adjust) {
+			temp = temp * probe.adjust;
+		}
+		temp = parseFloat(temp).toFixed(1);
+
+		if (callback) {
+			callback(channel, temp);
+		}
+	});
+};
