634c44185eb90e3b1e94f69026d9828b61e00981
Tobias Liebig <mail@etobi.de>
2016-08-26 09:10:50 +0200

    [TASK] fixes test

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 
 var shield = require('./index.js');
 
-shield.readAdcValue(0, function (value) {
-	console.log(value);
+shield.readAdcValue(0, function (channel, value) {
+	console.log('Ch#0: ' + value);
+});
+
+shield.readAdcValue(1, function (channel, value) {
+	console.log('Ch#1: ' + value);
 });
