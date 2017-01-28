f857d25588db33cad73f4d2518f68984d1375ee8
Tobias Liebig <work@etobi.de>
2017-01-28 18:02:52 +0100

    [TASK] add methods to control leds

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ var fs = require('fs');
 var McpAdc = require('mcp-adc');
 var adc;
 
+var Gpio = require('onoff').Gpio,
+  led1 = new Gpio(14, 'out'),
+  led2 = new Gpio(15, 'out'),
+  led3 = new Gpio(18, 'out'),
+  led4 = new Gpio(27, 'out');
+
 try {
     fs.accessSync('/dev/spidev0.0', fs.F_OK);
 
@@ -50,4 +56,29 @@ exports.readAdcTemp = function (probe, callback) {
 			callback(channel, temp);
 		}
 	});
+};
+
+exports.led1On = function() {
+	led1.writeSync(1);
+};
+exports.led1Off = function() {
+	led1.writeSync(0);
+};
+exports.led2On = function() {
+	led2.writeSync(1);
+};
+exports.led2Off = function() {
+	led2.writeSync(0);
+};
+exports.led3On = function() {
+	led3.writeSync(1);
+};
+exports.led3Off = function() {
+	led3.writeSync(0);
+};
+exports.led4On = function() {
+	led4.writeSync(1);
+};
+exports.led4Off = function() {
+	led4.writeSync(0);
 };
